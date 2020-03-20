aba2c716197821adb2bb6de27a5428b5f0f8fc9c
Oooom <omishah999@gmail.com>
2020-03-20 23:09:01 +0530

    initial commit

---
+++ car.js
@@ -0,0 +1,454 @@
+// NOTE: 
+//      getFitness of the car is directly bound with "m"
+//      dead() is bound to population, generation and dead lists
+
+
+var CAR_SENSOR_TRANSFORMATION_MATRIX = new THREE.Matrix4()
+
+var CAR_SENSOR_ROTATION_MATRICES = []
+var degrees = [180, 0, 90, -90, 0, 45, -45]
+for(var i = 0; i < degrees.length; i++){
+    var m = new THREE.Matrix4()
+    m.makeRotationFromEuler(new THREE.Euler(0, 0, to_radians(degrees[i])))
+
+    CAR_SENSOR_ROTATION_MATRICES.push( m )
+}
+
+var CAR_SENSOR_IDENTITY_MAT = new THREE.Matrix4()
+
+
+var HALF_EXTENTS_CAR_DIM = new CANNON.Vec3(1.5, 1, 0.5)
+var CAR_SENSOR_NOT_TRIGGERED_COLOR = 0xffff00
+var CAR_SENSOR_TRIGGERED_COLOR = 0xff0000
+var CAR_BODY_CHASSIS_SHAPE = new CANNON.Box(HALF_EXTENTS_CAR_DIM)
+var CAR_SENSOR_POSITION_VECTOR = new THREE.Vector3()
+
+var CAR_SENSOR_SIZE = 6.5
+
+
+var MAX_STEER_VAL = 0.5;
+var MAX_FORCE = 650;
+var BRAKE_FORCE = 1000000;
+
+var CAR_COLOR = 0x00FFFF
+var CAR_OPACITY = 0
+var CAR_HIGHLIGHT_COLOR = 0xFFC0CB
+var CAR_HIGHLIGHT_OPACITY = 1
+
+var THROTTLE_INDEX = 0
+var NO_THROTTLE_INDEX = 1
+var BRAKE_INDEX = 2
+var STEER_LEFT_INDEX = 3
+var NO_STEER_INDEX = 4
+var STEER_RIGHT_INDEX = 5
+
+function CarWithSensors() {
+    this.inWhichChunk = 0
+
+    this.disabled = false
+    this.throttleInput = 0
+    this.steerInput = 0.5
+    this.reverseInput = 0
+
+    this.sensors = []
+    this.lastCheckpointIndex = -1
+
+    //to detect whether the car has crashed into the wall or not
+    this.boundingPoints = [
+        new THREE.Vector3(HALF_EXTENTS_CAR_DIM.x, HALF_EXTENTS_CAR_DIM.y, HALF_EXTENTS_CAR_DIM.z),
+        new THREE.Vector3(HALF_EXTENTS_CAR_DIM.x, -HALF_EXTENTS_CAR_DIM.y, HALF_EXTENTS_CAR_DIM.z),
+        new THREE.Vector3(-HALF_EXTENTS_CAR_DIM.x, -HALF_EXTENTS_CAR_DIM.y, HALF_EXTENTS_CAR_DIM.z),
+        new THREE.Vector3(-HALF_EXTENTS_CAR_DIM.x, HALF_EXTENTS_CAR_DIM.y, HALF_EXTENTS_CAR_DIM.z)
+        // new THREE.Vector3(HALF_EXTENTS_CAR_DIM.x, HALF_EXTENTS_CAR_DIM.y, -HALF_EXTENTS_CAR_DIM.z),
+        // new THREE.Vector3(HALF_EXTENTS_CAR_DIM.x, -HALF_EXTENTS_CAR_DIM.y, -HALF_EXTENTS_CAR_DIM.z),
+        // new THREE.Vector3(-HALF_EXTENTS_CAR_DIM.x, -HALF_EXTENTS_CAR_DIM.y, -HALF_EXTENTS_CAR_DIM.z),
+        // new THREE.Vector3(-HALF_EXTENTS_CAR_DIM.x, HALF_EXTENTS_CAR_DIM.y, -HALF_EXTENTS_CAR_DIM.z)        
+    ]
+    var boundingPointsClone = []
+    this.boundingPoints.forEach((value)=> { boundingPointsClone.push(value.clone()) })
+
+    //sensors here
+    for(var i = 0; i < CAR_SENSOR_ROTATION_MATRICES.length; i++){
+        var sensor = new CarSensor(CAR_SENSOR_SIZE, 0, 0)
+        sensor.setRotationMatrix(CAR_SENSOR_ROTATION_MATRICES[i])
+
+        this.sensors.push(sensor)
+    }
+
+    //wheel options
+    var options = {
+        radius: 0.4,
+        directionLocal: new CANNON.Vec3(0, 0, -1),
+        suspensionStiffness: 30,
+        suspensionRestLength: 0.3,
+        frictionSlip: 5,
+        dampingRelaxation: 2.3,
+        dampingCompression: 4.4,
+        maxSuspensionForce: 100000,
+        rollInfluence: 0.01,
+        axleLocal: new CANNON.Vec3(0, 1, 0),
+        chassisConnectionPointLocal: new CANNON.Vec3(1, 1, 0),
+        maxSuspensionTravel: 0.3,
+        customSlidingRotationalSpeed: -30,
+        useCustomSlidingRotationalSpeed: true
+    }
+
+    this.mesh = new THREE.Mesh(
+        new THREE.BoxBufferGeometry(HALF_EXTENTS_CAR_DIM.x * 2, HALF_EXTENTS_CAR_DIM.y * 2, HALF_EXTENTS_CAR_DIM.z * 2),
+        new THREE.MeshPhongMaterial({
+            color: 0x00FFFF,
+            transparent: true,
+            opacity: CAR_OPACITY
+        })
+    )
+    global.THREE.scene.add(this.mesh)
+
+    this._wheelBodies = [];    
+    this.resetBody = _resetBody
+
+    this.tick = function () {
+        for (var i = 0; i < this._vehicle.wheelInfos.length; i++) {
+            this._vehicle.updateWheelTransform(i);
+            var t = this._vehicle.wheelInfos[i].worldTransform;
+
+            var wheelBody = this._wheelBodies[i];
+            wheelBody.position.copy(t.position);
+            wheelBody.quaternion.copy(t.quaternion);
+        }
+
+        for (var i = 0; i < this.sensors.length; i++) {
+            this.sensors[i].tick(this.mesh)
+        }
+    }
+
+    this.startTriggerToSensor = function (no) {
+        this.sensors[no].startTrigger()
+    }
+
+    this.endTriggerToSensor = function (no) {
+        this.sensors[no].endTrigger()
+    }
+
+    this.getBoundingPoints = function(){        
+        for(var i = 0; i < this.boundingPoints.length; i++){
+            boundingPointsClone[i].set(this.boundingPoints[i].x, this.boundingPoints[i].y, this.boundingPoints[i].z)
+
+            boundingPointsClone[i].applyMatrix4(this.mesh.matrix)
+        }
+
+        return boundingPointsClone
+    }
+
+    this.getFitness = function(){        
+        return (this.lastCheckpointIndex+1) / m.checkpoints.length 
+    }
+
+
+    //this is to be used only by the neural net
+    // -ve goes ahead and +ve goes back
+    this.applyEngineForce = function(force){
+        this._vehicle.applyEngineForce(force, 2);
+        this._vehicle.applyEngineForce(force, 3);
+    }
+
+    // -ve goes right and +ve goes left 
+    this.turn = function(steerVal){
+        this._vehicle.setSteeringValue(steerVal, 0);
+        this._vehicle.setSteeringValue(steerVal, 1);
+    }
+
+    this.forward = function(end){
+        this._vehicle.applyEngineForce(end ? 0 : -MAX_FORCE, 2);
+        this._vehicle.applyEngineForce(end ? 0 : -MAX_FORCE, 3);
+    }
+
+    this.reverse = function (end){
+        this._vehicle.applyEngineForce(end ? 0 : MAX_FORCE, 2);
+        this._vehicle.applyEngineForce(end ? 0 : MAX_FORCE, 3);
+    }
+
+    this.brake = function(force){
+        this._vehicle.setBrake(force, 0);
+        this._vehicle.setBrake(force, 1);
+        this._vehicle.setBrake(force, 2);
+        this._vehicle.setBrake(force, 3);
+    }
+
+    this.turnRight = function (end){
+        this._vehicle.setSteeringValue(end ? 0 : -MAX_STEER_VAL, 0);
+        this._vehicle.setSteeringValue(end ? 0 : -MAX_STEER_VAL, 1);
+    }
+
+    this.turnLeft = function (end){
+        this._vehicle.setSteeringValue(end ? 0 : MAX_STEER_VAL, 0);
+        this._vehicle.setSteeringValue(end ? 0 : MAX_STEER_VAL, 1);
+    }
+
+    this.disable = function(){
+        this.disabled = true
+
+        global.CANNON.world.removeBody(this.body)
+        this._vehicle.removeFromWorld(global.CANNON.world)
+    }
+    this.enable = function(){
+        this.disabled = false
+
+        global.CANNON.world.addBody(this.body)
+        this._vehicle.addToWorld(global.CANNON.world)
+    }
+
+    this.dead = function () {
+        //so that the wheels and sensor meshes are not aage peeche wrt the vehicle
+        this.mesh.position.set(this.body.position.x, this.body.position.y, this.body.position.z)
+        this.tick()
+
+        this.removeBodyAndVehicle()
+
+        var car_no = CarsBatcher.car_batch.indexOf(this)
+        population.splice(population.indexOf(car_no), 1)
+    }
+
+    this.removeBodyAndVehicle = function(){
+        global.CANNON.world.remove(this.body)
+        this._vehicle.removeFromWorld(global.CANNON.world)
+
+        var obj = global.MeshBodyPairs.find((pair) => {
+            return (pair.body == this.body && pair.mesh == this.mesh)
+        })
+        global.MeshBodyPairs.splice(global.MeshBodyPairs.indexOf(obj), 1)
+
+        this.body = null
+        this._vehicle = null
+    }
+
+    this.delete = function () {
+        global.THREE.scene.remove(this.mesh)
+        global.CANNON.world.remove(this.body)
+
+        this._vehicle.removeFromWorld(global.CANNON.world)
+
+        var obj = global.MeshBodyPairs.find((pair)=>{ 
+            return (pair.body == this.body && pair.mesh == this.mesh)
+        })
+        global.MeshBodyPairs.splice(global.MeshBodyPairs.indexOf(obj), 1)
+
+        var first_wheel_body = global.MeshBodyPairs.find((pair) => {
+            return (pair.body == this._wheelBodies[0])
+        })
+        var wheel_start = global.MeshBodyPairs.indexOf(first_wheel_body)
+        
+        for(var i = wheel_start; i < wheel_start+4; i++){
+            var pair = global.MeshBodyPairs[i]
+
+            global.CANNON.world.removeBody(pair.body)
+            global.THREE.scene.remove(pair.mesh)
+        }
+
+        global.MeshBodyPairs.splice(wheel_start, 4)
+
+        for(var sensor of this.sensors){
+            sensor.delete()
+        }
+    }
+
+    this.makeMove = function(){
+        var ips = [...this.sensors.map((sensor) => { return sensor.state / CAR_SENSOR_SIZE})]
+        ips.push(this.throttleInput)
+        ips.push(this.steerInput)
+        ips.push(this.reverseInput)
+
+        var res = this.brain.predict(ips)
+
+        var engine_force
+        var steer_val
+
+        this.throttleInput = 0
+        this.reverseInput = 0
+        
+        var max_val = Math.max(res[THROTTLE_INDEX], res[NO_THROTTLE_INDEX], res[BRAKE_INDEX])
+        if (max_val == res[THROTTLE_INDEX]){
+        
+            engine_force = -MAX_FORCE
+            this.throttleInput = 1
+        
+        }else if(max_val == res[NO_THROTTLE_INDEX]){
+            
+            engine_force = 0
+            this.throttleInput = 0
+
+        }else if(max_val == res[BRAKE_INDEX]){ 
+
+            engine_force = MAX_FORCE/2
+            this.reverseInput = 1
+
+        }
+
+        var max_val = Math.max(res[STEER_LEFT_INDEX], res[STEER_LEFT_INDEX], res[NO_STEER_INDEX])
+        if(max_val == res[STEER_RIGHT_INDEX]){
+        
+            steer_val = -MAX_STEER_VAL
+            this.steerInput = 1
+        
+        }if(max_val == res[STEER_LEFT_INDEX]){
+        
+            steer_val = 0
+            this.steerInput = 0.5
+        
+        }if(max_val == res[NO_STEER_INDEX]){
+        
+            steer_val = MAX_STEER_VAL
+            this.steerInput = 0
+        
+        }
+        
+
+        this.applyEngineForce(engine_force)
+        this.turn(steer_val)
+    }
+
+    function _resetBody(){
+        if(this.body != null) return
+
+        this.body = new CANNON.Body({
+            shape: CAR_BODY_CHASSIS_SHAPE,
+            mass: 150,
+            collisionFilterGroup: GROUP2,
+            collisionFilterMask: GROUP1
+        })
+        this.body.quaternion.set(-1, 0, 0, 1)
+        global.CANNON.world.add(this.body)
+    
+        global.MeshBodyPairs.push({
+            mesh: this.mesh,
+            body: this.body
+        })
+
+        this._vehicle = new CANNON.RaycastVehicle({
+            chassisBody: this.body,
+            collisionFilterGroup: GROUP2,
+            collisionFilterMask: GROUP1
+        })
+
+        options.chassisConnectionPointLocal.set(1, 1, 0)
+        this._vehicle.addWheel(options)
+
+        options.chassisConnectionPointLocal.set(1, -1, 0)
+        this._vehicle.addWheel(options)
+
+        options.chassisConnectionPointLocal.set(-1, 1, 0)
+        this._vehicle.addWheel(options)
+
+        options.chassisConnectionPointLocal.set(-1, -1, 0)
+        this._vehicle.addWheel(options)
+
+        this._vehicle.addToWorld(global.CANNON.world)
+
+        if(this._wheelBodies.length != 0) return
+
+        for (var i = 0; i < this._vehicle.wheelInfos.length; i++) {
+            var wheel = this._vehicle.wheelInfos[i];
+            var cylinderShape = new CANNON.Cylinder(wheel.radius, wheel.radius, wheel.radius / 2, 20);
+
+            var wheelBody = new CANNON.Body({
+                mass: 0,
+                collisionFilterGroup: 0,
+                collisionFilterMask: 0
+            });
+            wheelBody.type = CANNON.Body.KINEMATIC;
+            wheelBody.collisionResponse = false
+
+            var q = new CANNON.Quaternion();
+            q.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), Math.PI / 2);
+            wheelBody.addShape(cylinderShape, new CANNON.Vec3(), q);
+            this._wheelBodies.push(wheelBody);
+
+            global.CANNON.world.addBody(wheelBody);
+
+            var wheelMesh = new THREE.Mesh(
+                new THREE.CylinderBufferGeometry(wheel.radius, wheel.radius, wheel.radius / 2, 20),
+                new THREE.MeshPhongMaterial({
+                    color: 0xFFFF00
+                })
+            )
+            global.THREE.scene.add(wheelMesh)
+
+            global.MeshBodyPairs.push({
+                mesh: wheelMesh,
+                body: wheelBody
+            })
+        }
+    }
+}
+
+//x,y,z determines the position of the second point of the line segment to be draw from the chassis center
+function CarSensor(x, y, z) {
+    this.state = 0
+    this.p1 = new THREE.Vector3()
+    this.p2 = new THREE.Vector3()
+
+    this.collisionPoint = new THREE.Mesh(new THREE.SphereBufferGeometry(0.1),
+                                         new THREE.MeshBasicMaterial({
+                                           color: 0xff0000
+                                         }))
+
+    global.THREE.scene.add(this.collisionPoint)
+
+    this.mesh = new THREE.Line(new THREE.Geometry(), new THREE.LineBasicMaterial({
+        color: CAR_SENSOR_NOT_TRIGGERED_COLOR
+    }))
+    global.THREE.scene.add(this.mesh)
+
+    this.mesh.geometry.vertices.push(new THREE.Vector3(0, 0, 0))
+    this.mesh.geometry.vertices.push(new THREE.Vector3(0, 0, 0))
+    this.transformationMatrix = CAR_SENSOR_TRANSFORMATION_MATRIX
+    this.position = CAR_SENSOR_POSITION_VECTOR
+    this.rotationMatrix = null
+
+    this.setRotationMatrix = function (rotMat) {
+        this.rotationMatrix = rotMat
+    }
+
+    this.tick = function (chassisMesh) {
+        if (this.rotationMatrix == null) {
+            throw new Error("CarSensor's Rotation Matrix not set")
+        }
+
+        this.position.set(0, 0, 0)
+        this.transformationMatrix.identity()
+
+        this.transformationMatrix.makeTranslation(x, y, z)
+        this.transformationMatrix.multiplyMatrices(this.rotationMatrix, this.transformationMatrix)
+        this.transformationMatrix.multiplyMatrices(chassisMesh.matrix, this.transformationMatrix)
+
+        this.position.applyMatrix4(this.transformationMatrix)
+
+        this.mesh.geometry.vertices[0].set(chassisMesh.position.x, chassisMesh.position.y, chassisMesh.position.z)
+        this.mesh.geometry.vertices[1].set(this.position.x, this.position.y, this.position.z)
+        this.mesh.geometry.verticesNeedUpdate = true
+
+        this.p1.set(chassisMesh.position.x, chassisMesh.position.y, chassisMesh.position.z)
+        this.p2.set(this.position.x, this.position.y, this.position.z)
+    }
+
+    this.startTrigger = function (value) {
+        if (this.state == 1) {
+            return;
+        }
+
+        this.state = value
+        this.mesh.material.color.setHex(CAR_SENSOR_TRIGGERED_COLOR)
+    }
+
+    this.endTrigger = function () {
+        if (this.state == 0) {
+            return;
+        }
+
+        this.state = CAR_SENSOR_SIZE
+        this.mesh.material.color.setHex(CAR_SENSOR_NOT_TRIGGERED_COLOR)
+    }
+
+    this.delete = function () {
+        global.THREE.scene.remove(this.mesh)
+    }
+}
---
+++ map.js
@@ -0,0 +1,411 @@
+var WALL_MATERIAL = new THREE.MeshBasicMaterial({
+    color: 0xaaaaaa,
+    side: THREE.DoubleSide
+})
+
+var CHECKPOINT_MATERIAL = new THREE.MeshBasicMaterial({
+    color: 0xa0a0fa,
+    side: THREE.DoubleSide
+})
+
+function highlightMesh(mesh) {
+    var old_pos = mesh.position.y
+
+    mesh.position.y = 10
+
+    setTimeout(function () {
+        mesh.position.y = old_pos
+    }, 1000)
+}
+
+function Map(start_pt, deg, width) {
+    this.walls = [{
+        walls: [],
+        bb: []
+    }] //making the 0th chunk here itself
+    this.wallHeight = 2
+
+    this.checkpoints = []
+    this.checkpointHeight = 0.5
+
+    this.width = width
+
+    var tmpRotMat = new THREE.Matrix4()
+    tmpRotMat.makeRotationFromEuler(new THREE.Euler(0, to_radians(deg), 0))
+
+
+    var tmpRay = []
+
+
+    //BB is Tp2, Tp1, Lp1, Bp2
+    //left
+    tmpRay.push({
+        p1: new THREE.Vector3(start_pt.x - width / 2, 0, start_pt.z + width / 2),
+        p2: new THREE.Vector3(start_pt.x - width / 2, 0, start_pt.z - width / 2),
+    })
+    //bottom
+    tmpRay.push({
+        p1: new THREE.Vector3(start_pt.x - width / 2, 0, start_pt.z + width / 2),
+        p2: new THREE.Vector3(start_pt.x + width / 2, 0, start_pt.z + width / 2),
+    })
+    //top
+    tmpRay.push({
+        p1: new THREE.Vector3(start_pt.x - width / 2, 0, start_pt.z - width / 2),
+        p2: new THREE.Vector3(start_pt.x + width / 2, 0, start_pt.z - width / 2),
+    })
+
+    for (var i = 0; i < tmpRay.length; i++) {
+        tmpRay[i].p1.applyMatrix4(tmpRotMat)
+        tmpRay[i].p2.applyMatrix4(tmpRotMat)
+
+        this.walls[0].walls.push(new Wall(tmpRay[i].p1, tmpRay[i].p2, this.wallHeight))
+    }
+
+    this.walls[0].bb = [
+        this.walls[0].walls[2].p2,
+        this.walls[0].walls[2].p1,
+        this.walls[0].walls[0].p1,
+        this.walls[0].walls[1].p2,
+    ]
+
+
+    this.leftCursor = this.walls[0].walls[1].p2.clone()
+    this.rightCursor = this.walls[0].walls[2].p2.clone()
+
+    //map creation helpers' helpers
+    this.extendRoute = function (x1, z1, x2, z2) {
+        this.extendLeftCursor(x1, z1)
+        this.extendRightCursor(x2, z2)
+
+        return this
+    }
+
+    this.extendLeftCursor = function (x, z) {
+        this.walls[this.walls.length - 1].walls.push(
+            new Wall(
+                new THREE.Vector3(this.leftCursor.x, 0, this.leftCursor.z),
+                new THREE.Vector3(this.leftCursor.x + x, 0, this.leftCursor.z + z),
+                this.wallHeight
+            )
+        )
+
+        this.leftCursor.x += x
+        this.leftCursor.z += z
+
+        return this
+    }
+
+    this.extendRightCursor = function (x, z) {
+        this.walls[this.walls.length - 1].walls.push(
+            new Wall(
+                new THREE.Vector3(this.rightCursor.x, 0, this.rightCursor.z),
+                new THREE.Vector3(this.rightCursor.x + x, 0, this.rightCursor.z + z),
+                this.wallHeight
+            )
+        )
+
+        this.rightCursor.x += x
+        this.rightCursor.z += z
+
+        return this
+    }
+
+    //these are the functions which should be called directly
+    this.extendRouteTurnRight = function (currentDir) {
+
+        this.walls.push({
+            walls: [],
+            bb: []
+        })
+
+        var just_created = this.walls[this.walls.length - 1]
+
+        var fourthVec = new THREE.Vector3(0, 0, 0)
+
+        switch (currentDir) {
+            case "N":
+                this.extendRightCursor(0, -this.width)
+                this.extendRightCursor(this.width, 0)
+
+                fourthVec.x = just_created.walls[1].p2.x
+                fourthVec.z = just_created.walls[0].p1.z
+                break
+
+            case "S":
+                this.extendRightCursor(0, this.width)
+                this.extendRightCursor(-this.width, 0)
+
+                fourthVec.x = just_created.walls[1].p2.x
+                fourthVec.z = just_created.walls[0].p1.z
+                break
+
+            case "W":
+                this.extendRightCursor(-this.width, 0)
+                this.extendRightCursor(0, -this.width)
+
+                fourthVec.x = just_created.walls[0].p1.x
+                fourthVec.z = just_created.walls[1].p2.z
+
+                break
+
+            case "E":
+                this.extendRightCursor(this.width, 0)
+                this.extendRightCursor(0, this.width)
+
+                fourthVec.x = just_created.walls[0].p1.x
+                fourthVec.z = just_created.walls[1].p2.z
+
+                break
+
+            default:
+                throw new Error("Unknown Direction")
+        }
+
+        just_created.bb = [
+            just_created.walls[0].p1,
+            just_created.walls[0].p2,
+            just_created.walls[1].p2,
+            fourthVec
+        ]
+    }
+
+    this.extendRouteTurnLeft = function (currentDir) {
+
+        this.walls.push({
+            walls: [],
+            bb: []
+        })
+        var just_created = this.walls[this.walls.length - 1]
+
+        var fourthVec = new THREE.Vector3(0, 0, 0)
+
+        switch (currentDir) {
+            case "N":
+                this.extendLeftCursor(0, -this.width)
+                this.extendLeftCursor(-this.width, 0)
+
+                fourthVec.x = just_created.walls[1].p2.x
+                fourthVec.z = just_created.walls[0].p1.z
+
+                break
+
+            case "S":
+                this.extendLeftCursor(0, this.width)
+                this.extendLeftCursor(this.width, 0)
+
+                fourthVec.x = just_created.walls[1].p2.x
+                fourthVec.z = just_created.walls[0].p1.z
+
+                break
+
+            case "W":
+                this.extendLeftCursor(-this.width, 0)
+                this.extendLeftCursor(0, this.width)
+
+                fourthVec.x = just_created.walls[0].p1.x
+                fourthVec.z = just_created.walls[1].p2.z
+
+                break
+
+            case "E":
+                this.extendLeftCursor(this.width, 0)
+                this.extendLeftCursor(0, -this.width)
+
+                fourthVec.x = just_created.walls[0].p1.x
+                fourthVec.z = just_created.walls[1].p2.z
+
+                break
+
+            default:
+                throw new Error("Unknown Direction")
+        }
+
+        just_created.bb = [
+            just_created.walls[0].p1,
+            just_created.walls[0].p2,
+            just_created.walls[1].p2,
+            fourthVec
+        ]
+    }
+
+    this.extendRouteStraight = function (units, current_dir) {
+
+        this.walls.push({
+            walls: [],
+            bb: []
+        })
+
+        switch (current_dir) {
+            case "E":
+                this.extendRoute(units, 0, units, 0)
+                break
+
+            case "W":
+                this.extendRoute(-units, 0, -units, 0)
+                break
+
+            case "N":
+                this.extendRoute(0, -units, 0, -units)
+                break
+
+            case "S":
+                this.extendRoute(0, units, 0, units)
+                break
+
+            default:
+                throw new Error("Unknown Direction")
+        }
+
+        var just_created = this.walls[this.walls.length - 1]
+
+        //w1p1 w1p2 w2p2 w2p1
+        just_created.bb = [
+            just_created.walls[0].p1,
+            just_created.walls[0].p2,
+            just_created.walls[1].p2,
+            just_created.walls[1].p1
+        ]
+    }
+
+    //checkpoint creation
+    this.addCheckpointAtCursor = function () {
+        this.checkpoints.push(new Wall(
+            new THREE.Vector3(this.leftCursor.x, 0, this.leftCursor.z),
+            new THREE.Vector3(this.rightCursor.x, 0, this.rightCursor.z),
+            this.checkpointHeight,
+            CHECKPOINT_MATERIAL
+        ))
+    }
+
+    //collision helpers
+    this.checkCollisions = function (vehicles) {
+        for (var vehicleNo of vehicles) {
+            if (this.checkCollisionForVehicle(CarsBatcher.car_batch[vehicleNo])) {
+                CarsBatcher.car_batch[vehicleNo].dead()
+            }
+        }
+    }
+
+    var chunk_list = []
+    this.checkCollisionForVehicle = function (vehicle) {
+        var bps = vehicle.getBoundingPoints()
+        var sensorState = [0, 0, 0, 0]
+        var isDead = false
+
+        chunk_list[0] = m.walls[vehicle.inWhichChunk]
+        chunk_list[1] = m.walls[vehicle.inWhichChunk - 1]
+        chunk_list[2] = m.walls[vehicle.inWhichChunk + 1]
+
+        var is_new_chunk_set = false
+        var i = 0
+        for (var chunk of chunk_list) {
+
+            if(chunk != undefined){
+
+                for(var wall of chunk.walls){
+                    //checking for vehicle body's collision with wall
+                    for (var i = 0; i < bps.length; i++) {
+                        var line = {
+                            p1: bps[i],
+                            p2: bps[(i + 1) % bps.length]
+                        }
+        
+                        if (!isDead) {
+                            isDead = isIntersect(line, wall)
+                        }
+                    }
+        
+                    if (isDead) {
+                        break
+                    }
+        
+                    //sensor collision checking
+                    for (var i = 0; i < vehicle.sensors.length; i++) {
+                        var poi = lineLineIntersection(vehicle.sensors[i], wall)
+        
+                        if (poi) {
+                            vehicle.sensors[i].collisionPoint.position.set(poi.x, vehicle.sensors[i].p1.y, poi.z)
+        
+                            var x = poi.x - vehicle.sensors[i].p1.x
+                            var z = poi.z - vehicle.sensors[i].p1.z
+        
+                            //@TODO - remove sqrt and directly divide with max distance sq
+                            var distance = Math.sqrt(x * x + z * z)
+                            sensorState[i] = distance
+                        }
+                    }
+        
+                    //checking for collision with checkpoint... disabled for now
+                    {
+                        // if (vehicle.lastCheckpointIndex < m.checkpoints.length - 1){
+                        //     var nextCheckpointForThisVehicle = m.checkpoints[vehicle.lastCheckpointIndex + 1]
+        
+                        //     for (var i = 0; i < bps.length; i++) {
+                        //         var line = {
+                        //             p1: bps[i],
+                        //             p2: bps[(i + 1) % bps.length]
+                        //         }
+        
+                        //         if ( isIntersect(line, nextCheckpointForThisVehicle) ){
+                        //             vehicle.lastCheckpointIndex++
+                        //             break
+                        //         }
+                        //     }
+                        // }
+                    }
+        
+                    for (var i = 0; i < vehicle.sensors.length; i++) {
+                        if (sensorState[i] > 0) {
+                            vehicle.sensors[i].startTrigger(sensorState[i])
+                        } else {
+                            vehicle.sensors[i].endTrigger()
+        
+                            vehicle.sensors[i].collisionPoint.position.set(vehicle.sensors[i].p2.x, vehicle.sensors[i].p1.y, vehicle.sensors[i].p2.z)
+                        }
+                    }
+                }
+                
+                
+                if(isDead){
+                    break
+                }
+                
+                if(!is_new_chunk_set){
+                    if(is_within_polygon(chunk.bb, vehicle.mesh.position)){
+                        vehicle.inWhichChunk = m.walls.indexOf(chunk)
+                        is_new_chunk_set = true
+                    }
+                }
+            }
+
+            i++;
+        }
+
+        return isDead
+    }
+}
+
+const _yAxisVec = new THREE.Vector3(0, 1, 0)
+
+function Wall(p1, p2, height, material) {
+    this.p1 = p1
+    this.p2 = p2
+
+    var x = p2.x - p1.x
+    var z = p2.z - p1.z
+    var length = Math.sqrt(x * x + z * z)
+
+    if (length != 0) {
+        if (material === undefined) {
+            material = WALL_MATERIAL
+        }
+
+        this.mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(length, height), material)
+
+        global.THREE.scene.add(this.mesh)
+        this.mesh.position.set(p1.x + length / 2, p1.y + height / 2, p1.z)
+
+        var theta = Math.atan2(z, x)
+        rotateAboutPoint(this.mesh, p1, _yAxisVec, -theta, true)
+    }
+}
